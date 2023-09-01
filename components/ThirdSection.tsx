a49fd15331e5e0f3733b22ba174339de4c037d43
Danila Frolov <frolovdan94@gmail.com>
2023-09-01 16:31:47 +0700

    Fix v2.2

--- components/ThirdSection.tsx
+++ components/ThirdSection.tsx
@@ -55,7 +55,7 @@ export const ThirdSection = () => {
                     </div>
                 </div>
             </div>
-            <div className='relative'>
+            <div id='id5' className='relative'>
                 <div className={style.thirdSection}></div>
                 <div className='px-4'>
                     <div className='max-w-7xl mx-auto p-4 flex flex-col lg:flex-row justify-between items-center lg:items-stretch gap-4'>
