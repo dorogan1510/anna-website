7d11bc22992cd342225c878459b6ce7c68de90c8
Danila Frolov <frolovdan94@gmail.com>
2023-09-07 16:43:37 +0700

    Fix 7 optimization

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { FourthSection } from '@/components/FourthSection'
 import { HeroSection } from '@/components/HeroSection'
 import { SecondSection } from '@/components/SecondSection'
 import { ThirdSection } from '@/components/ThirdSection'
+import { Metadata } from 'next'
 
 export default function Home() {
     return (
